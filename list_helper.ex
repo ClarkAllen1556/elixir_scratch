eaf8c4f020505ea6b5a9162d90c7688b5391af3a
ClarkAllen1556 <allenclark@u.boisestate.edu>
2021-12-16 13:31:46 +0900

    tail recusion length function

--- list_helper.ex
+++ list_helper.ex
@@ -7,6 +7,17 @@ defmodule ListHelper do
   #   head + sum(tail)
   # end
 
+  def length(list), do: p_length(0, list)
+
+  defp p_length(count, []) do
+    count
+  end
+
+  defp p_length(count, [head | tail]) do
+    count + 1
+    |> p_length(tail)
+  end
+
   def sum(list) do
     p_sum(0, list)
   end
